a9d9a5dd2ae72d765bea111a340afb44af8f79ab
Junior Navarro <41093728+ljrnavarro@users.noreply.github.com>
2020-04-19 21:56:46 -0300

    Router, Json Dependences

--- projectschool/src/components/Professor/Professor.vue
+++ projectschool/src/components/Professor/Professor.vue
@@ -12,13 +12,13 @@
       <tbody v-if="professores.length">
         <tr v-for="(professor, index) in professores" :key="index">
           <td>{{ professor.id }}</td>
-         
-            <router-link to="/alunos/" tag="td" style="cursor: pointer">
-              {{ professor.nome }} {{ professor.sobrenome }}
-            </router-link>
-          
+
+          <router-link to="/alunos/" tag="td" style="cursor: pointer">
+            {{ professor.nome }} {{ professor.sobrenome }}
+          </router-link>
+
           <td>
-            3
+            {{getQtdAlunosPorProfessor(professor.id)}}
           </td>
         </tr>
       </tbody>
@@ -34,17 +34,39 @@ import Titulo from "../_share/Titulo";
 import VueResource from "vue-resource";
 export default {
   components: {
-    Titulo
+    Titulo,
   },
   data() {
     return {
-      professores: [
-        { id: 1, nome: "Vinicius", sobrenome: "Silva" },
-        { id: 2, nome: "Francisco", sobrenome: "Xavier" },
-        { id: 3, nome: "Luis", sobrenome: "Dantas" }
-      ]
+      professores: [],
+      alunos: []
     };
-  }
+  },
+  created() {
+   this.$http
+      .get("http://localhost:3000/alunos")
+      .then((res) => res.json())
+      .then((alunos) => {
+        this.alunos = alunos;
+        this.carregarProfessores();
+        });
+  },
+  props: {},
+  methods: {
+    getQtdAlunosPorProfessor(id) {
+          return this.alunos.filter(aluno => 
+          aluno.professor.id == id
+          ).length;
+        },
+    carregarProfessores() {
+      this.$http
+        .get("http://localhost:3000/professores")
+        .then((res) => res.json())
+        .then((professor) => {
+          this.professores = professor;
+          });
+    },
+  },
 };
 </script>
 
